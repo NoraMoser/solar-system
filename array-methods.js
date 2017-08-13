c7753df4b560d046b4e0933bd3905304cf0f3426
Nora Moser <noramoser@Noras-MBP.lan>
2017-08-13 10:08:19 -0500

    first letter in first planet capitalized so far

--- array-methods.js
+++ array-methods.js
@@ -5,18 +5,27 @@ var planets = ["mercury", " venus", " earth", " mars", " jupiter", " saturn", " 
  to a div element in your HTML with an id of "planets".
 */
 var el = document.getElementById("planets");
+var el2 = document.getElementById("planets2");
 let planetDom = "";
+let planetDom2 = "";
 
 planets.forEach(function(element) {
     planetDom = `<h3>${planets}</h3>`
     console.log(element);
     el.innerHTML = planetDom;
-})
+});
 
 // Use the map method to create a new array where the first letter of each planet is capitalized
+
+planets.forEach(function(item) {
+	let plans = item;
+	let capFirst = plans.charAt(0).toUpperCase() + plans.slice(1);
+	
+	el2.innerHTML += capFirst + " ";
+})
 
 // Use the filter method to create a new array that contains planets with the letter 'e'
 
 // Use the reduce method to create a sentence from the words in the following array
 
-var words = ["The", "early", "bird", "might", "get", "the", "worm", "but", "the", "second", "mouse", "gets", "the", "cheese"];
+// var words = ["The", "early", "bird", "might", "get", "the", "worm", "but", "the", "second", "mouse", "gets", "the", "cheese"];
